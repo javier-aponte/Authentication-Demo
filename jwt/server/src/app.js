ebfdfd104af4623ef41be0164fc3fff1f043a607
Javier Aponte <31341776+javier-aponte@users.noreply.github.com>
2023-11-29 10:22:41 -0500

    jwt server - cors added and configured

--- jwt/server/src/app.js
+++ jwt/server/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -12,6 +13,13 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: [
+    "http://localhost",
+    "http://localhost:5173",
+  ],
+  credentials: true
+}))
 
 app.use('/auth', AuthRouter);
 app.use('/', HomeRouter);
